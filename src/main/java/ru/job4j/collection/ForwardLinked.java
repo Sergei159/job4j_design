ca46e0b34aad4bd909a2f376bca9cf4f4c282b3c
sergei159 <89082766060QAzQAz>
2021-10-13 17:44:57 +0500

    Удалить head в односвязном списке. [#51424 #215169]

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -1,28 +1,25 @@
 package ru.job4j.collection;
 
-
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class ForwardLinked<T> implements Iterable<T> {
-    private int modCount = 0;
-    private int size = 0;
+    private int modCount;
+    private int size;
     private Node<T> head;
 
     public void add(T value) {
         Node<T> node = new Node<T>(value, null);
         if (head == null) {
             head = node;
-            size++;
-            modCount++;
-            return;
+        } else {
+            Node<T> tail = head;
+            while (tail.next != null) {
+                tail = tail.next;
+            }
+            tail.next = node;
         }
-        Node<T> tail = head;
-        while (tail.next != null) {
-            tail = tail.next;
-        }
-        tail.next = node;
         size++;
         modCount++;
     }
