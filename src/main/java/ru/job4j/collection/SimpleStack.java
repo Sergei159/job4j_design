270b71f25bb102761769d228a6a2e6382beb3c53
sergei159 <89082766060QAzQAz>
2021-10-15 15:27:47 +0500

    Используя контейнер на базе связанного списка создать контейнер Stack [#71474]

---
+++ src/main/java/ru/job4j/collection/SimpleStack.java
@@ -0,0 +1,53 @@
+package ru.job4j.collection;
+
+import java.util.NoSuchElementException;
+import java.util.LinkedList;
+
+public class SimpleStack<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private  ForwardLinked<T>  linked = new ForwardLinked<T>();
+
+    public T pop() {
+        if (tail == null) {
+            throw new NoSuchElementException();
+        }
+        final T deleted = tail.value;
+        if (tail == head) {
+            head = null;
+            tail = null;
+        } else {
+            Node<T> newTail = head;
+            while (newTail.next != tail) {
+                newTail = newTail.next;
+            }
+            newTail.next = null;
+            tail = newTail;
+        }
+        return deleted;
+
+    }
+
+    public void push(T value) {
+        Node<T> node = new Node<T>(value, null);
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+    }
+
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+}
+
