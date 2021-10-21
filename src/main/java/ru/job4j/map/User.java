e57e50c14975341d69a3e95236da6939cf57397d
Sergei159 <qars2010@yandex.ru>
2021-10-22 00:34:50 +0500

    Реализовать коллекцию Set на массиве [#996]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,6 @@
 package ru.job4j.map;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
     private String name;
@@ -13,5 +13,49 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "name='" + name + '\''
+                + ", children=" + children
+                + ", birthday=" + birthday
+                + '}';
+    }
+
+    private static void printCollection(Collection collection) {
+        for (Object obj : collection) {
+            System.out.println(obj.toString());
+        }
+    }
+
+    public static void main(String[] args) {
+        Map<User, Object> users = new HashMap<User, Object>();
+        Calendar dot = Calendar.getInstance();
+        dot.set(1980, 12, 20);
+        User user1 = new User("user1", 2, dot);
+        User user2 = new User("user1", 2, dot);
+        users.put(user1, new Object());
+        users.put(user2, new Object());
+        printCollection(users.entrySet());
+
     }
 }
