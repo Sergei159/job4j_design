0513df9bd98fcc5088aaadb14672929a894216f6
sergei159 <89082766060QAzQAz>
2021-11-18 16:45:17 +0500

    Config

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -1,35 +1,51 @@
 package ru.job4j.io;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringJoiner;
-
+/**
+ * This class loads a file by this.path and save into a map pairs
+ * split by "="
+ */
 public class Config {
-
     private final String path;
     private final Map<String, String> values = new HashMap<String, String>();
-
     public Config(final String path) {
         this.path = path;
     }
-
+    /**
+     * loads the file from this.path and save the relevant pairs  into
+     * the Map<String, String> values by splitting with "="
+     * Strings mustn't be a comment
+     */
     public void load() {
-        StringJoiner out = new StringJoiner(System.lineSeparator());
-        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
-
-            read.lines().forEach(out::add);
+        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
+            in.lines()
+                    .filter(s ->  s.length() > 0 && !s.startsWith("#"))
+                    .forEach(str -> {
+                        String[] pair = str.split("=");
+                        if (pair.length != 2) {
+                            throw new IllegalArgumentException();
+                        }
+                        values.put(pair[0], pair[1]);
+                    });
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
+    /**
+     *
+     * @param key Key of the value to be found
+     * @return value to be associated with the specified key
+     * of the Map<String, String> if founded
+     * otherwise throws UnsupportedOperationException
+     * @throws UnsupportedOperationException
+     */
     public String value(String key) {
-        throw new UnsupportedOperationException("Don't impl this method yet!");
+        return values.get(key);
     }
-
     @Override
     public String toString() {
         StringJoiner out = new StringJoiner(System.lineSeparator());
@@ -42,7 +58,6 @@ public class Config {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Config("app.properties"));
     }
 
-}
+} 
