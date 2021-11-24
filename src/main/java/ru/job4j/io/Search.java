b7f082fe96d001c62d22cde00c55ef85470418c0
sergei159 <89082766060QAzQAz>
2021-11-24 15:08:12 +0500

    Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -11,6 +11,7 @@ import java.util.function.Predicate;
  * Class search files by a predicate and output them in a console
  */
 
+
 public class Search  {
     public static void main(String[] args) throws IOException {
         if (args.length != 2) {
