7100b8322f0b9d618a4dd4dcb86518423a32b3e7
sergei159 <89082766060QAzQAz>
2022-01-10 14:54:22 +0500

    Бот [#7921 #248823]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -14,22 +14,21 @@ public class EchoServer {
 
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
-            boolean isWorking = true;
-            while (isWorking) {
+            while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
-                        if (str.contains("Hello")) {
+                        if (str.contains("msg=Hello")) {
                             out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                             out.write("Hello\r\n\r\n".getBytes());
                             break;
-                        } else if (str.contains("Exit")) {
+                        } else if (str.contains("msg=Exit")) {
                             out.write("HTTP/1.1 522\r\n\r\n".getBytes());
                             out.write("Bye\r\n\r\n".getBytes());
-                            isWorking = false;
+                            server.close();
                             break;
                         } else {
                             out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
