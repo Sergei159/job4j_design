a4a860deafc3daf7ac602152bceb795ff7217a54
Sergei159 <qars2010@yandex.ru>
2022-01-12 00:00:19 +0500

    JAXB. Преобразование XML в POJO. [#315063]

--- src/main/java/ru/job4j/serialization/Student.java
+++ src/main/java/ru/job4j/serialization/Student.java
@@ -1,14 +1,32 @@
 package ru.job4j.serialization;
 
 import java.util.Arrays;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.*;
+import java.io.StringWriter;
 
+@XmlRootElement(name = "student")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Student {
+
+    @XmlAttribute
     private boolean isStudying;
+
+    @XmlAttribute
     private int age;
+
+    @XmlAttribute
     private String name;
+
     private Contact studentContact;
+
+    @XmlElementWrapper
+    @XmlElement(name = "achievement")
     private String[] achievements;
 
+    public Student() { }
 
     public Student(boolean isStudying, int age, String name,
                    Contact studentContact, String[] achievements) {
