1cb5aff2e15454674001450c9dddd2b2d97aa38e
Sergei159 <qars2010@yandex.ru>
2022-02-13 23:36:00 +0500

    Что такое TDD? [#4918 #263287]

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -3,12 +3,14 @@ package ru.job4j.tdd;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 
+@Ignore
 public class CinemaTest {
 
     @Test
@@ -37,5 +39,37 @@ public class CinemaTest {
         List<Session> sessions = cinema.find(session -> true);
         assertThat(sessions, is(Arrays.asList((new Session3D("Name1")), (new Session3D("Name2")))));
 
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void invalidPlace() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2022, 02, 11, 16, 06);
+        Ticket ticket = cinema.buy(account, 0, 1, date);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void invalidDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 02, 11, 16, 06);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenPlaceIsOccupied() {
+        Account account1 = new AccountCinema();
+        Account account2 = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date1 = Calendar.getInstance();
+        date1.set(2022, 02, 11, 16, 00);
+        Ticket ticket1 = cinema.buy(account1, 1, 1, date1);
+
+        Calendar date2 = Calendar.getInstance();
+        date2.set(2022, 02, 11, 18, 00);
+        Ticket ticket2 = cinema.buy(account2, 1, 1, date2);
     }
 }
