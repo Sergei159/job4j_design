f3a00af745e3d93a18f77087956e8d7c0a24c4cc
Sergei159 <qars2010@yandex.ru>
2022-02-17 21:00:08 +0500

    Парковка машин [#853 #265222]

--- src/test/java/ru/job4j/parking/MixedParkTest.java
+++ src/test/java/ru/job4j/parking/MixedParkTest.java
@@ -59,4 +59,34 @@ public class MixedParkTest {
         assertThat(mixedPark.get(), is(expected));
     }
 
+    @Test
+    public void whenTruckPlacesAreOccupied() {
+        MixedPark mixedPark = new MixedPark(3, 1);
+        Transport car1 = new GeneralCar();
+        Transport truck1 = new Truck(3);
+        Transport truck2 = new Truck(3);
+        mixedPark.park(car1);
+        mixedPark.park(truck1);
+        mixedPark.park(truck2);
+
+        List<Transport> expected = List.of(
+                car1, truck1);
+        assertThat(mixedPark.get(), is(expected));
+    }
+
+    @Test
+    public void whenGeneralCarPlacesAreOccupied() {
+        MixedPark mixedPark = new MixedPark(2, 1);
+        Transport car1 = new GeneralCar();
+        Transport car2 = new GeneralCar();
+        Transport car3 = new GeneralCar();
+        mixedPark.park(car1);
+        mixedPark.park(car2);
+        mixedPark.park(car3);
+
+        List<Transport> expected = List.of(
+                car1, car2);
+        assertThat(mixedPark.get(), is(expected));
+    }
+
 }
